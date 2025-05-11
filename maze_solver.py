f2fd278dada61060b2f2a912084f0a9e19b7e850
Justin Theodorus <justin.theodorus@gmail.com>
2025-05-11 10:12:20 +0800

    Created Maze class

--- maze_solver.py
+++ maze_solver.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+import time
 
 class Point:
     def __init__(self, x, y):
@@ -70,6 +71,39 @@ class Cell:
         end_x = (to_cell._x1 + to_cell._x2) // 2
         end_y = (to_cell._y1 + to_cell._y2) // 2
         self._win.draw_line(Line(Point(start_x, start_y), Point(end_x, end_y)), color)
+
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self.x1 = x1
+        self.y1 = y1
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.cell_size_x = cell_size_x
+        self.cell_size_y = cell_size_y
+        self.win = win
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self.num_cols):
+            column = []
+            for j in range(self.num_rows):
+                x1 = self.x1 + i * self.cell_size_x
+                y1 = self.y1 + j * self.cell_size_y
+                x2 = x1 + self.cell_size_x
+                y2 = y1 + self.cell_size_y
+                cell = Cell(x1, y1, x2, y2, self.win)
+                column.append(cell)
+                self._draw_cell(i, j)
+            self._cells.append(column)
+
+    def _draw_cell(self, i, j):
+        self._cells[i][j].draw()
+        self._animate()
+
+    def _animate(self):
+        self.win.redraw()
+        time.sleep(0.05)
 
 def main():
     win = Window(800, 600)
