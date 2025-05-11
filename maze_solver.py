3d58ffda68354fe121f52fe004c2b53dac206b69
Justin Theodorus <justin.theodorus@gmail.com>
2025-05-11 10:18:13 +0800

    Created break walls method

--- maze_solver.py
+++ maze_solver.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 import time
+import random
 
 class Point:
     def __init__(self, x, y):
@@ -53,6 +54,7 @@ class Cell:
         self._y1 = y1
         self._y2 = y2
         self._win = win
+        self.visited = False
 
     def draw(self):
         if self._win is not None:
@@ -83,7 +85,7 @@ class Cell:
             self._win.draw_line(Line(Point(start_x, start_y), Point(end_x, end_y)), color)
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
         self.x1 = x1
         self.y1 = y1
         self.num_rows = num_rows
@@ -92,6 +94,8 @@ class Maze:
         self.cell_size_y = cell_size_y
         self.win = win
         self._cells = []
+        if seed is not None:
+            random.seed(seed)
         self._create_cells()
 
     def _create_cells(self):
@@ -116,6 +120,41 @@ class Maze:
         if self.win is not None:
             self.win.redraw()
             time.sleep(0.05)
+
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            directions = []
+            if i > 0 and not self._cells[i - 1][j].visited:
+                directions.append((-1, 0))
+            if i < self.num_cols - 1 and not self._cells[i + 1][j].visited:
+                directions.append((1, 0))
+            if j > 0 and not self._cells[i][j - 1].visited:
+                directions.append((0, -1))
+            if j < self.num_rows - 1 and not self._cells[i][j + 1].visited:
+                directions.append((0, 1))
+
+            if not directions:
+                self._draw_cell(i, j)
+                return
+
+            di, dj = random.choice(directions)
+            ni, nj = i + di, j + dj
+
+            if di == -1:
+                self._cells[i][j].has_left_wall = False
+                self._cells[ni][nj].has_right_wall = False
+            elif di == 1:
+                self._cells[i][j].has_right_wall = False
+                self._cells[ni][nj].has_left_wall = False
+            elif dj == -1:
+                self._cells[i][j].has_top_wall = False
+                self._cells[ni][nj].has_bottom_wall = False
+            elif dj == 1:
+                self._cells[i][j].has_bottom_wall = False
+                self._cells[ni][nj].has_top_wall = False
+
+            self._break_walls_r(ni, nj)
 
     def _break_entrance_and_exit(self):
         # Break the entrance at the top-left cell
