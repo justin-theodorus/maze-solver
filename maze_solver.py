255694306ba8d26c91d2cae9fa8ae4cf9ab5ec9d
Justin Theodorus <justin.theodorus@gmail.com>
2025-05-11 10:10:17 +0800

    Added Cell class with some tests on cell configurations

--- maze_solver.py
+++ maze_solver.py
@@ -41,19 +41,42 @@ class Window:
     def draw_line(self, line, fill_color):
         line.draw(self.__canvas, fill_color)
 
+class Cell:
+    def __init__(self, x1, y1, x2, y2, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = win
+
+    def draw(self):
+        if self.has_left_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x1, self._y2)), "black")
+        if self.has_right_wall:
+            self._win.draw_line(Line(Point(self._x2, self._y1), Point(self._x2, self._y2)), "black")
+        if self.has_top_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y1), Point(self._x2, self._y1)), "black")
+        if self.has_bottom_wall:
+            self._win.draw_line(Line(Point(self._x1, self._y2), Point(self._x2, self._y2)), "black")
+
 def main():
     win = Window(800, 600)
     
-    # Test drawing lines
-    p1 = Point(100, 100)
-    p2 = Point(200, 200)
-    line1 = Line(p1, p2)
-    win.draw_line(line1, "black")
+    # Test drawing cells
+    cell1 = Cell(50, 50, 100, 100, win)
+    cell1.draw()
 
-    p3 = Point(200, 100)
-    p4 = Point(300, 200)
-    line2 = Line(p3, p4)
-    win.draw_line(line2, "red")
+    cell2 = Cell(150, 50, 200, 100, win)
+    cell2.has_right_wall = False
+    cell2.draw()
+
+    cell3 = Cell(250, 50, 300, 100, win)
+    cell3.has_bottom_wall = False
+    cell3.draw()
 
     win.wait_for_close()
 
